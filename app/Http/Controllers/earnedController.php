3000a6687667298e5e5e78e91fc7fd8949457210
quangtung1412 <quangtung1412@gmail.com>
2021-07-10 08:48:45 +0700

    Update earnedController.php

--- app/Http/Controllers/earnedController.php
+++ app/Http/Controllers/earnedController.php
@@ -104,61 +104,95 @@ class earnedController extends Controller
                 $acc_index = 0;
                 $sumVertical = 0;
                 foreach ($accounts as $acc) {
-                    $address = str_replace('ronin:', '0x', $acc->ronin);
-                    $url = "https://lunacia.skymavis.com/game-api/clients/".$address."/items/1";
-                    $options = array(
-                        CURLOPT_RETURNTRANSFER => true,     // return web page
-                        CURLOPT_HEADER         => false,    // don't return headers
-                        CURLOPT_FOLLOWLOCATION => true,     // follow redirects
-                        CURLOPT_ENCODING       => "",       // handle all encodings
-                        CURLOPT_USERAGENT      => "spider", // who am i
-                        CURLOPT_AUTOREFERER    => true,     // set referer on redirect
-                        CURLOPT_CONNECTTIMEOUT => 120,      // timeout on connect
-                        CURLOPT_TIMEOUT        => 120,      // timeout on response
-                        CURLOPT_MAXREDIRS      => 10,       // stop after 10 redirects
-                        CURLOPT_SSL_VERIFYPEER => false     // Disabled SSL Cert checks
-                    );
+                    // $address = str_replace('ronin:', '0x', $acc->ronin);
+                    // $url = "https://lunacia.skymavis.com/game-api/clients/".$address."/items/1";
+                    // $options = array(
+                    //     CURLOPT_RETURNTRANSFER => true,     // return web page
+                    //     CURLOPT_HEADER         => false,    // don't return headers
+                    //     CURLOPT_FOLLOWLOCATION => true,     // follow redirects
+                    //     CURLOPT_ENCODING       => "",       // handle all encodings
+                    //     CURLOPT_USERAGENT      => "spider", // who am i
+                    //     CURLOPT_AUTOREFERER    => true,     // set referer on redirect
+                    //     CURLOPT_CONNECTTIMEOUT => 120,      // timeout on connect
+                    //     CURLOPT_TIMEOUT        => 120,      // timeout on response
+                    //     CURLOPT_MAXREDIRS      => 10,       // stop after 10 redirects
+                    //     CURLOPT_SSL_VERIFYPEER => false     // Disabled SSL Cert checks
+                    // );
 
-                    $ch      = curl_init( $url );
-                    curl_setopt_array( $ch, $options );
-                    $content = curl_exec( $ch );
-                    $err     = curl_errno( $ch );
-                    $errmsg  = curl_error( $ch );
-                    $header  = curl_getinfo( $ch );
-                    curl_close( $ch );
+                    // $ch      = curl_init( $url );
+                    // curl_setopt_array( $ch, $options );
+                    // $content = curl_exec( $ch );
+                    // $err     = curl_errno( $ch );
+                    // $errmsg  = curl_error( $ch );
+                    // $header  = curl_getinfo( $ch );
+                    // curl_close( $ch );
 
-                    $header['errno']   = $err;
-                    $header['errmsg']  = $errmsg;
-                    $header['content'] = $content;
-                    // dd($header);
-                    $res = json_decode($header['content']);
-                    if($res){
-                        // die;
-                        // dd($res);die;
-                        $curBalance = intval($res->total);
-                        $claimable = intval($res->claimable_total);
-                        $last_claimed = $res->last_claimed_item_at;
-                        $acc->claimable = $claimable;
-                        $acc->total = $curBalance;
-                        $acc->last_claimed = $last_claimed;
-                        $acc->save();
+                    // $header['errno']   = $err;
+                    // $header['errmsg']  = $errmsg;
+                    // $header['content'] = $content;
+                    // // dd($header);
+                    // $res = json_decode($header['content']);
+                    // if($res){
+                    //     // die;
+                    //     // dd($res);die;
+                    //     $curBalance = intval($res->total);
+                    //     $claimable = intval($res->claimable_total);
+                    //     $last_claimed = $res->last_claimed_item_at;
+                    //     $acc->claimable = $claimable;
+                    //     $acc->total = $curBalance;
+                    //     $acc->last_claimed = $last_claimed;
+                    //     $acc->save();
 
-                        // echo $curBalance;die;
-                        $day_yesterday = date('d',strtotime("-1 days"));
-                        $month_yesterday = date('Ym',strtotime("-1 days"));
-                        $bal_yesterday = balance_eod::where('acc_id', $acc->id)->where('month_id', $month_yesterday)->where('day', $day_yesterday)->first();
-                        if($bal_yesterday)
-                            $curEarned = $curBalance - $bal_yesterday->balance;
-                        else
-                            $curEarned = $curBalance;
-                        $earned[] = $curEarned;
-                        $sum[$acc_index] += $curEarned;
-                        $sumVertical += $curEarned;
-                    }else{
-                        $earned[] = "ERROR";
-                        // $sum[$acc_index] += 0;
-                        // $sumVertical += 0;
+                    //     // echo $curBalance;die;
+                    //     $day_yesterday = date('d',strtotime("-1 days"));
+                    //     $month_yesterday = date('Ym',strtotime("-1 days"));
+                    //     // $bal_yesterday = balance_eod::where('acc_id', $acc->id)->where('month_id', $month_yesterday)->where('day', $day_yesterday)->first();
+                    //     $bal_today = balance_eod::where('acc_id', $acc->id)->where('month_id', date('Ym'))->where('day', date('d'))->first();
+                    //     if($bal_today)
+                    //         $curEarned = $bal_today->earned;
+                    //     else{
+                    //         // if($bal_yesterday){
+                    //         //     $diff_bal = $curBalance - $bal_yesterday->balance;
+                    //         //     if($diff_bal > 0)
+                    //         //         $curEarned = $diff_bal;
+                    //         //     else
+                    //         //         $curEarned = 0;
+                    //         // }
+                    //         // else
+                    //         //     $curEarned = $curBalance;
+                    //         $curEarned = 0;
+                    //     }
+                        
+                    //     $earned[] = $curEarned;
+                    //     $sum[$acc_index] += $curEarned;
+                    //     $sumVertical += $curEarned;
+                    // }else{
+                    //     $earned[] = "ERROR";
+                    //     // $sum[$acc_index] += 0;
+                    //     // $sumVertical += 0;
+                    // }
+
+
+
+                    $bal_today = balance_eod::where('acc_id', $acc->id)->where('month_id', date('Ym'))->where('day', date('d'))->first();
+                    if($bal_today)
+                        $curEarned = $bal_today->earned;
+                    else{
+                        // if($bal_yesterday){
+                        //     $diff_bal = $curBalance - $bal_yesterday->balance;
+                        //     if($diff_bal > 0)
+                        //         $curEarned = $diff_bal;
+                        //     else
+                        //         $curEarned = 0;
+                        // }
+                        // else
+                        //     $curEarned = $curBalance;
+                        $curEarned = 0;
                     }
+                    
+                    $earned[] = $curEarned;
+                    $sum[$acc_index] += $curEarned;
+                    $sumVertical += $curEarned;
                     $acc_index++;
                 }
                 $sumAll += $sumVertical;
